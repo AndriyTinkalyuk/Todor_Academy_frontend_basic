953eeee57afc4b938e01ff81bc96baf50ed8532e
AndriyTinkalyuk <tinkalukandriy@gmail.com>
2025-01-20 16:19:35 +0200

    add animation intro

--- lesson 024-025/script/script.js
+++ lesson 024-025/script/script.js
@@ -5,3 +5,12 @@ let menuBody = document.querySelector('.header_nav');
 let tools = document.querySelector('.header_tools')
 
 burgerMenu(iconMenu, menuBody, tools);
+
+// потрібно переробити в модуль
+
+let introBlock = document.querySelector('.welcome');
+document.querySelector('.draw').addEventListener('animationend', (e) =>  {
+        introBlock.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    
+});
